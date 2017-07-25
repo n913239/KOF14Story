bb68e2ce4197d2a15b6540bb86aa34c5767e3013
n913239 <n913239@gmail.com>
2017-07-25 21:22:55 +0800

    Initial Commit

---
+++ KOF14Story/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  KOF14Story
+//
+//  Created by mikewang on 2017/7/25.
+//  Copyright © 2017年 mike. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
